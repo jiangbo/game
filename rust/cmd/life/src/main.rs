1bf6182052b663b6e63964a328d37c560d00df5c
JiangBo <1006758852@qq.com>
2022-06-02 15:46:49 +0800

    Update main.rs

--- rust/cmd/life/src/main.rs
+++ rust/cmd/life/src/main.rs
@@ -1,42 +1,94 @@
-use std::io::Write;
-use std::{io, time::Duration};
-
-use crossterm::{cursor, event, terminal, ExecutableCommand};
-use crossterm::{event::KeyCode, style::Print, QueueableCommand};
-use life::Universe;
+use crossterm::{cursor, event, style::Print, terminal};
+use crossterm::{ExecutableCommand, QueueableCommand};
+use game::Universe;
+use std::{fmt::Display, io::Write, time::Duration};
 
 fn main() -> crossterm::Result<()> {
-    let (cols, rows) = terminal::size()?;
-    let (width, height) = ((cols / 2) as u32, rows as u32);
-    let mut universe = Universe::new(width, height);
-
-    io::stdout()
-        .queue(terminal::EnterAlternateScreen)?
-        .queue(terminal::SetTitle("Conway's Life Game"))?
-        .queue(terminal::Clear(terminal::ClearType::All))?
-        .queue(cursor::Hide)?;
-
-    let mut goon = true;
+    let mut world = World::new()?;
     loop {
-        if event::poll(Duration::from_millis(10))? {
-            if let event::Event::Key(event) = event::read()? {
-                match event.code {
-                    KeyCode::Esc => break,
-                    KeyCode::Enter => goon = !goon,
-                    _ => (),
-                }
-            }
+        if event::poll(Duration::from_millis(500))? {
+            handle_event(&mut world, event::read()?);
         }
 
-        if goon {
-            universe.tick();
-            io::stdout()
-                .queue(terminal::Clear(terminal::ClearType::All))?
-                .queue(Print(&universe))?
-                .flush()?;
+        match world.status {
+            Status::Over => return Ok(()),
+            Status::Pause => (),
+            Status::Running => world.render()?,
         }
     }
+}
 
-    io::stdout().execute(terminal::LeaveAlternateScreen)?;
-    Ok(())
+fn handle_event(world: &mut World, event: event::Event) {
+    use event::{KeyCode, KeyModifiers};
+    if let event::Event::Key(key) = event {
+        let status = match (key.code, key.modifiers) {
+            (KeyCode::Esc, _) => Status::Over,
+            (KeyCode::Char('c'), KeyModifiers::CONTROL) => Status::Over,
+            (KeyCode::Char(' '), _) => match world.status {
+                Status::Running => Status::Pause,
+                _ => Status::Running,
+            },
+            _ => return,
+        };
+        world.status = status;
+    }
+}
+
+struct World {
+    description: &'static str,
+    status: Status,
+    universe: Universe,
+}
+
+pub enum Status {
+    Running,
+    Pause,
+    Over,
+}
+
+impl World {
+    fn new() -> crossterm::Result<Self> {
+        let (cols, rows) = World::create_terminal()?;
+        let (width, height) = (cols as u32, rows as u32);
+        Ok(World {
+            description: "Space for pause, Esc and Ctrl + c for exit",
+            status: Status::Running,
+            universe: Universe::new(width / 2, height - 3),
+        })
+    }
+
+    fn create_terminal() -> crossterm::Result<(u16, u16)> {
+        terminal::enable_raw_mode()?;
+        std::io::stdout().execute(cursor::Hide)?;
+        terminal::size()
+    }
+
+    fn render(&mut self) -> crossterm::Result<()> {
+        self.universe.tick();
+        std::io::stdout()
+            .queue(terminal::Clear(terminal::ClearType::All))?
+            .queue(Print(self))?
+            .flush()
+    }
+
+    fn reset_terminal() -> crossterm::Result<()> {
+        std::io::stdout()
+            .execute(terminal::Clear(terminal::ClearType::All))?
+            .execute(cursor::Show)?;
+        terminal::enable_raw_mode()
+    }
+}
+
+impl Drop for World {
+    fn drop(&mut self) {
+        if let Err(e) = World::reset_terminal() {
+            log::error!("drop world error: {}", e);
+        }
+    }
+}
+
+impl Display for World {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}\n\n{}", self.description, self.universe)
+    }
 }
